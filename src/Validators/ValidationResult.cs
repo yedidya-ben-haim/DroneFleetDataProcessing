21956e743e308e78d34f63482fdb31f8c971dcbe
yedidya ben haim <bhyedidya@gmail.com>
2026-07-14 17:56:49 +0300

    Implementing the DroneValidator class

--- src/Validators/ValidationResult.cs
+++ src/Validators/ValidationResult.cs
@@ -3,10 +3,9 @@ namespace DroneFleetDataProcessing.Validators
     public class ValidationResult
     {
         public List<Drone> ValidDrones { get; }
-
         public int RejectedCount { get; }
 
-        public ValidationResult List<Drone> validDrones, int rejectedCount)
+        public ValidationResult (List<Drone> validDrones, int rejectedCount)
         {
             ValidDrones = validDrones;
             RejectedCount = rejectedCount;
