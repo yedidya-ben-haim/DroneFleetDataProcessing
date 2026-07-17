6116ed7f4733b08cba6601c028be2be85ebebe76
yedidya ben haim <bhyedidya@gmail.com>
2026-07-17 12:03:49 +0300

    Save local ProcessPipeline changes

--- src/Pipeline/ProcessPipeline.cs
+++ src/Pipeline/ProcessPipeline.cs
@@ -184,9 +184,10 @@ public class ProcessPipeline
 
         fileLogger.log("MODEL WITH HIGHEST TOTAL COMPLETED MISSIONS");
         ReportGenerator.ShowModelWithHighestCompletedMissions(fileLogger, cleanDrones, analyzer);
+        fileLogger.log("");
 
         fileLogger.log("SELECTED ADDITIONAL ANALYSIS");
-        fileLogger.log($"ANALYSISAnalysis name: Top Three Models By Average Flight Hours");
+        fileLogger.log($"Analysis name: Top Three Models By Average Flight Hours");
         ReportGenerator.ShowTopThreeModelsByAverageFlightHours(fileLogger, cleanDrones, analyzer);
 
     }
